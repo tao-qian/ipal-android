4b6557e581a6e053c691b283a3d03253309c8136
Tao Qian <taoqian_2015@depauw.edu>
2012-07-03 21:26:11 +0800

    testing

--- ipal-android/src/com/ipalandroid/IpalAndroidActivity.java
+++ ipal-android/src/com/ipalandroid/IpalAndroidActivity.java
@@ -8,6 +8,6 @@ public class IpalAndroidActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main); //test
     }
 }
