79c5aed2d39f0c53e27c1f845109ed2625f17b7b
Nick Nguyen <nguyenhuungoc92@gmail.com>
2013-02-25 16:33:03 -0500

    Fix obscure Text Color in EditText

--- ipal-android/src/com/ipalandroid/questionview/MutipleChoiceQuesionView.java
+++ ipal-android/src/com/ipalandroid/questionview/MutipleChoiceQuesionView.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
